4d82356adac0a280c94d8b8b1cdfaa9c817e378e
Oli <55298769+xMistt@users.noreply.github.com>
2020-09-03 17:52:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import asyncio
+import os
+import uvloop
+
+os.system('pip install -U PartyBotPackage==0.1.9')
+os.system('clear')
+
 import PartyBotPackage
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 client = PartyBotPackage.PartyBot(
-    device_id="",
-    account_id="",
-    secret=""
+    device_id=os.getenv('DEVICE_ID'),
+    account_id=os.getenv('ACCOUNT_ID'),
+    secret=os.getenv('SECRET')
 )
 
 client.run()

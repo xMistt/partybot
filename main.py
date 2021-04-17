4b457f220897815e5a17a821218b117b9b2b1b2f
github.com/xMistt <76889012+xMistt2@users.noreply.github.com>
2021-04-17 17:15:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import asyncio
 import os
 import sys
 
-if not os.getenv('DEVICE_ID') and \
-        not os.getenv('ACCOUNT_ID') and \
-        not os.getenv('SECRET'):
-    print("Please paste your device auths into the \".env\" file.\n"
-          "If you're confused, re-watch the tutorial.")
-    sys.exit()
+env_vars = {}
+if os.path.isfile('.auth'):
+    with open('.auth') as f:
+        for line in f:
+            if line.startswith('#') or not line.strip():
+                continue
+            key, value = line.strip().split('=', 1)
+            env_vars[key] = value.replace("\"", "")
+else:
+  print("You're missing the \".auth\" file, if you have a \".env\" file rename it to \".auth\".")
+  sys.exit()
 
 os.system('pip install certifi')
 os.system('pip install -U PartyBotPackage')
@@ -19,9 +24,9 @@ import PartyBotPackage
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 client = PartyBotPackage.PartyBot(
-    device_id=os.getenv('DEVICE_ID'),
-    account_id=os.getenv('ACCOUNT_ID'),
-    secret=os.getenv('SECRET')
+    device_id=env_vars['DEVICE_ID'],
+    account_id=env_vars['ACCOUNT_ID'],
+    secret=env_vars['SECRET']
 )
 
 try:
@@ -31,3 +36,4 @@ except Exception as e:
     print("Failed to login, your device auths are probably invalid, please "
           "try again and make new ones.\nIf you're confused, re-watch the "
           "tutorial.")
+    print(env_vars)
